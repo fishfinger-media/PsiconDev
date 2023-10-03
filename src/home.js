377d1cd242aa25791a8a214de5965f2e72b26bda
fishfinger-media <122613283+fishfinger-media@users.noreply.github.com>
2023-10-03 14:13:48 +0100

    changes

--- src/home.js
+++ src/home.js
@@ -3,16 +3,79 @@ import './styles/style.css'
 console.log('COUNTRY ROAD TAKE ME HOME')
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import SplitType from 'split-type';
+import Swiper from 'swiper';
 
-gsap.registerPlugin(ScrollTrigger);
-let animationDuration = 0.8;
 
-let homeText = new SplitType("[home-heading]")
+// SERVICE SLIDER 
 
-gsap.timeline()
-  .from(".section_home-hero", {opacity:0, yPercent: 10, duration:animationDuration })
-  .from(homeText.chars, {opacity:0, yPercent: 40, stagger: {amount: 0.6}, ease:"back", duration: animationDuration}, animationDuration/2)
-  .from("[home-fade]", {opacity:0, yPercent: 25, ease:"back", duration:animationDuration},animationDuration/2)
-  .from("[home-image]", {opacity:0, yPercent: 5, ease:"back", duration:animationDuration*2},animationDuration/2)
+// SWIPER
+const serviceFinder = new Swiper(".swiper", {
+  wrapperClass: "swiper_wrapper",
+  slideClass: "swiper_slide",
+  slidesPerView: 1,
+  allowTouchMove: false,
+  speed: 0,
+});
 
+// SWIPER TRANSITION
+function fadeOutAndSlideNext() {
+  // Find the element with the specified class
+  const elementToFadeOut = document.querySelector('.swiper_slide.swiper-slide-active');
+
+  if (elementToFadeOut) {
+      // Set the opacity to 0 over 1000ms (1 second)
+      elementToFadeOut.style.transition = 'opacity 500ms';
+      elementToFadeOut.style.opacity = 0;
+
+      // Wait for the animation to complete, then call serviceFinder.slideNext()
+      setTimeout(function () {
+          serviceFinder.slideNext();
+      }, 500);
+  } else {
+      // If the element is not found, just call serviceFinder.slideNext() without animation
+      serviceFinder.slideNext();
+  }
+}
+
+// OPEN SERVICE PICKER
+const serviceNextElements = document.querySelectorAll("[service-open]");
+serviceNextElements.forEach(function (element) {
+  element.addEventListener("click", function () {
+      fadeOutAndSlideNext()
+  });
+});
+
+// BACK BUTTON
+const serviceBackElements = document.querySelectorAll("[service-back]");
+serviceBackElements.forEach(function (element) {
+  element.addEventListener("click", function () {
+      serviceFinder.slidePrev();
+  });
+});
+
+// URL GENERATION
+let help = '';
+let person = '';
+let nhs = '';
+
+function updateURL() {
+  const url = `/services/${help}/${person}`;
+  window.location.href = url;
+}
+
+// Function to preload the URL
+function preloadURL() {
+  const url = `/services/${help}/${person}`;
+  const a = document.createElement('a');
+  a.href = url;
+  a.style.display = 'none'; 
+  document.body.appendChild(a);
+}
+
+document.getElementById('service-btn-autism').addEventListener('click', function () { help = 'autism'; fadeOutAndSlideNext() });
+document.getElementById('service-btn-adhd').addEventListener('click', function () { help = 'adhd'; fadeOutAndSlideNext() });
+document.getElementById('service-btn-wellbeing').addEventListener('click', function () { help = 'wellbeing'; fadeOutAndSlideNext() });
+document.getElementById('service-btn-adults').addEventListener('click', function () { person = 'adults'; preloadURL(); fadeOutAndSlideNext() });
+document.getElementById('service-btn-children').addEventListener('click', function() { person = 'children'; preloadURL(); fadeOutAndSlideNext() });
+document.getElementById('service-btn-yes').addEventListener('click', function() { nhs = 'yes'; updateURL(); });
+document.getElementById('service-btn-no').addEventListener('click', function() { nhs = 'no'; updateURL(); });
--- src/main.js
+++ src/main.js
@@ -6,19 +6,34 @@ import SplitType from 'split-type';
 import Lenis from '@studio-freight/lenis'
 import Swiper from 'swiper';
 
-
+// LENIS
 const lenis = new Lenis()
-
-
 function raf(time) {
   lenis.raf(time)
   requestAnimationFrame(raf)
 }
-
 requestAnimationFrame(raf)
 
+// GSAP
+let animationDuration = 0.8;
+let homeText = new SplitType("[home-heading]")
 
+gsap.timeline()
+  .from(".section_home-hero", {opacity:0, yPercent: 10, duration:animationDuration })
+  .from(homeText.chars, {opacity:0, yPercent: 40, stagger: {amount: 0.6}, ease:"back", duration: animationDuration}, animationDuration/2)
+  .from("[home-fade]", {opacity:0, yPercent: 25, ease:"back", duration:animationDuration},animationDuration/2)
+  .from("[home-image]", {opacity:0, yPercent: 5, ease:"back", duration:animationDuration*2},animationDuration/2)
 
+document.querySelectorAll("[data-section]").forEach((section) => {
+  gsap.from(section, {opacity: 0, y: 128, ease: "power4.out", duration: animationDuration, delay:0.2,
+  scrollTrigger: {
+    trigger: section,
+    start: "top 80%",
+    end: "bottom 80%",
+    toggleActions: "play none none none"
+  }
+  });
+});
 
 console.log('Im Home')
 
